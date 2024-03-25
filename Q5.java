cb60f49195c71d25cabbad15ab50eff37be4cbbe
Abhiram-Laha <abhiramlaha2020@gmail.com>
2024-03-25 20:49:35 +0530

    ok

--- Q5.java
+++ Q5.java
@@ -1,5 +1,20 @@
 // Greatest of the Three numbers
 
+import java.util.Scanner;
+
 public class Q5 {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter : ");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        int temp = a>b ? a:b;
+
+        int res = temp>c ? temp:c;
+
+        System.out.println("Greatest No. : "+res);
+
+    }
 }
